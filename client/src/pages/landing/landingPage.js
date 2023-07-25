67c809fd870246883acc9f914fd690346e433780
josh Watson <joshjkwatson@gmail.com>
2023-07-25 17:52:10 +1000

    updated landing page to go back to login on logout

--- client/src/pages/landing/landingPage.js
+++ client/src/pages/landing/landingPage.js
@@ -1,12 +1,13 @@
 import React from "react";
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 export const LandingPage = () => {
     return(
         <header className="header">
             <h1 className="headingTitle"> Welcome to my Quoting app.</h1>
             <div>
-                <button className="startBtn"> Free Quote </button>
+                <button className="startBtn"> Start Free Quote </button>
+                <button className="loginBtn"> <Link to="/"></Link> Log out here</button>
                 </div>
 
         </header>
