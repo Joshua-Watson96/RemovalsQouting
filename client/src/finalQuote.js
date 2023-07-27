579595d79299e7eed5ec2a7d07a642a49b783154
Joshua Watson <joshjkwatson@gmail.com>
2023-07-27 20:33:57 +1000

    refactor code readability

--- client/src/finalQuote.js
+++ client/src/finalQuote.js
@@ -3,20 +3,19 @@ import React from "react";
 export const QuoteReview = () => {
   let pickUpAddress = JSON.parse(localStorage.getItem("pickUpAddress"));
   let dropOffAddress = JSON.parse(localStorage.getItem("dropOffAddress"));
-  let bedroomQuestion = localStorage.getItem("bedroomsTotal");
-  let pickupQuestion = +localStorage.getItem("pickupTotal");
-  let dropoffQuestion = +localStorage.getItem("dropOffTotal");
-  let truckAccessQuestion = +localStorage.getItem("accessTotal");
+  let bedroomsTotal = localStorage.getItem("bedroomsTotal");
+  let pickupTotal = +localStorage.getItem("pickupTotal");
+  let dropOffTotal = +localStorage.getItem("dropOffTotal");
+  let truckAccessToal = +localStorage.getItem("accessTotal");
 
-  console.log(pickupQuestion, dropoffQuestion, truckAccessQuestion);
-
-  let total = (pickupQuestion + dropoffQuestion + truckAccessQuestion) * 195;
+  
+  let total = (pickupTotal + dropOffTotal + truckAccessToal) * 195;
   return (
     <div>
       <ul>
         <li>Pick Up Address: {pickUpAddress}</li>
         <li>Drop Off Address:{dropOffAddress}</li>
-        <li>Number of Bedrroms: {bedroomQuestion}</li>
+        <li>Number of Bedrroms: {bedroomsTotal}</li>
         {/* <li>Pick Up Access: {pickupQuestion} </li>
             <li>Drop Off Access: {dropoffQuestion}</li>
             <li>Truck Access: {truckAccessQuestion}</li> */}
